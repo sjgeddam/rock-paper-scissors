9699db03514ee9230986092941b7fcc6119e142f
Soujanya Geddam <sgeddam@utexas.edu>
2022-06-25 16:36:34 -0500

    Write clear func header

--- scripts.js
+++ scripts.js
@@ -19,26 +19,28 @@ function playRPS(playerSelection, computerSelection) {
 
     if (playerSelection == "ROCK") {
         if (computerSelection == "PAPER") {
-            console.log(printWinOrLose(false, playerSelection, computerSelection)); //lose
+            console.log(WinOrLose(false, playerSelection, computerSelection)); //lose
             return 0; 
         } else if (computerSelection == "SCISSORS") {
-            console.log(printWinOrLose(true, playerSelection, computerSelection)); //win 
+            console.log(WinOrLose(true, playerSelection, computerSelection)); //win 
             return 1; 
         }
     }
 
     if (playerSelection == "PAPER") {
         if (computerSelection == "SCISSORS") {
-            console.log(printWinOrLose(false, playerSelection, computerSelection)); // lose
+            console.log(WinOrLose(false, playerSelection, computerSelection)); // lose
             return 0; 
         } else if (computerSelection == "ROCK") {
-            console.log(printWinOrLose(true, playerSelection, computerSelection)); // win
+            console.log(WinOrLose(true, playerSelection, computerSelection)); // win
             return 1; 
         }
     }
+
+    return 0; 
 }
 
-function printWinOrLose(wongame, playerSelection, computerSelection) {
+function WinOrLose(wongame, playerSelection, computerSelection) {
     if (wongame) {
         return "YOU WIN! You selected: " + playerSelection + " and computer selected: " + computerSelection;
     } else {
