7f4bc4c857384906312e611605dfef0763ef826c
Soujanya Geddam <sgeddam@utexas.edu>
2022-07-09 15:52:34 -0500

    Add UI to Rock Paper Scissors game

--- scripts.js
+++ scripts.js
@@ -1,6 +1,5 @@
 function computerPlay() {
     let choice = Math.ceil(Math.random(3) * 3);
-
     switch (choice) {
         case 1:
             return "ROCK"
@@ -11,28 +10,29 @@ function computerPlay() {
     }
 }
 
-function playRPS(playerSelection, computerSelection) {
+function playRound(playerSelection, computerSelection) {
+    const resdiv = document.querySelector('#results');
     if (playerSelection == computerSelection) {
-        console.log("THERE'S A DRAW! You both selected: " + playerSelection);
+        resdiv.textContent = "THERE'S A DRAW! You both selected: " + playerSelection;
         return 0; 
     }
 
     if (playerSelection == "ROCK") {
         if (computerSelection == "PAPER") {
-            console.log(WinOrLose(false, playerSelection, computerSelection)); //lose
+            resdiv.textContent = WinOrLose(false, playerSelection, computerSelection); //lose
             return 0; 
         } else if (computerSelection == "SCISSORS") {
-            console.log(WinOrLose(true, playerSelection, computerSelection)); //win 
+            resdiv.textContent = WinOrLose(true, playerSelection, computerSelection); //win 
             return 1; 
         }
     }
 
     if (playerSelection == "PAPER") {
         if (computerSelection == "SCISSORS") {
-            console.log(WinOrLose(false, playerSelection, computerSelection)); // lose
+            resdiv.textContent = WinOrLose(false, playerSelection, computerSelection); // lose
             return 0; 
         } else if (computerSelection == "ROCK") {
-            console.log(WinOrLose(true, playerSelection, computerSelection)); // win
+            resdiv.textContent = WinOrLose(true, playerSelection, computerSelection); // win
             return 1; 
         }
     }
@@ -49,19 +49,19 @@ function WinOrLose(wongame, playerSelection, computerSelection) {
 }
 
 function game() {
-    let gamesWon = 0; 
-    for (let i = 0; i < 5; i++) {
-        let playerSelection = prompt("Choose ROCK, PAPER, or SCISSORS").toUpperCase();
-        let computerSelection = computerPlay();
-        gamesWon += playRPS(playerSelection, computerSelection);   
-    }
-    if (gamesWon >= 3) {
-        console.log("You won " + gamesWon + " out of the 5 games! You win overall!!!");
-    } else {
-        console.log("You won " + gamesWon + " out of the 5 games! You lost overall!!!");
-    }
+    const rockB = document.querySelector('#rock');
+    const paperB = document.querySelector('#paper');
+    const scissorsB = document.querySelector('#scissors');
+    rockB.addEventListener('click', () => {
+        playRound("ROCK", computerPlay());
+    });
+    paperB.addEventListener('click', () => {
+        playRound("PAPER", computerPlay());
+    });
+    scissorsB.addEventListener('click', () => {
+        playRound("SCISSORS", computerPlay());
+    });
 }
 
-game();
-
+game(); 
 
